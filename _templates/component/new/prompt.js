a75a0270314aedadf7f8f84f8d45a2831a078bbf
Alex Eden <alexandereden91@gmail.com>
2020-08-04 18:12:55 -0500

    Add hygen templates

---
+++ _templates/component/new/prompt.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'ComponentName',
+    message: 'What\'s the name of the component? (in PascalCase)',
+    initial: 'MyNewComponent',
+  },
+];
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,11 +3,10 @@
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test';
-    readonly PUBLIC_URL: string;
-  }
+declare module '*.scss' {
+  const styles: { readonly [className: string]: string };
+
+  export default styles;
 }
 
 declare module '*.bmp' {
