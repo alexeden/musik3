3acc56cf260ed3ba0a6ef27d78f677eb4d3541a6
Alex Eden <alexandereden91@gmail.com>
2020-08-05 00:39:47 -0500

    EYYY I see time and frequency domain data

--- src/components/MusikControls/MusikControls.tsx
+++ src/components/MusikControls/MusikControls.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 export const MusikControls: React.FC<Props> = props => {
-  const { load, isLoading, } = useMusik();
+  const { load, isLoading, ...musik } = useMusik();
 
   useEffect(() => void load('/hallucinate.mp3'), []); // eslint-disable-line
 
@@ -14,6 +14,8 @@ export const MusikControls: React.FC<Props> = props => {
     <div>
       <h1>MusikControls</h1>
       <button onClick={() => load('/hallucinate.mp3')}>{isLoading ? '...' : 'Load'}</button>
+      <button onClick={() => musik.play()}>Play</button>
+      <button onClick={() => musik.pause()}>Pause</button>
     </div>
   );
 };
--- src/hooks/useMusik.ts
+++ src/hooks/useMusik.ts
@@ -1,5 +1,12 @@
-import { useRef, useState, } from 'react';
+import {
+  useRef, useState, useCallback, useEffect,
+} from 'react';
+import { useFrame, } from 'react-three-fiber';
 import { useRefFunctions, } from './useRefFunctions';
+
+const levelsCount = 16;
+const BEAT_MIN = 0.15; // level less than this is no beat
+const msecsAvg = 633; // time between beats (msec)
 
 const useAudioAnalyzer = (() => {
   const context = new AudioContext();
@@ -14,31 +21,62 @@ const useAudioAnalyzer = (() => {
 
 const useAudioBufferSourceNode = () => {
   const { analyzer, context, } = useAudioAnalyzer();
-  const sourceRef = useRef<AudioBufferSourceNode | null>(null);
+  const sourceRef = useRef<AudioBufferSourceNode>(context.createBufferSource());
 
   return {
     source: sourceRef.current,
     connect: (buffer: AudioBuffer) => {
       sourceRef.current?.disconnect();
-      const source = context.createBufferSource();
-      source.connect(analyzer);
-      source.buffer = buffer;
-      source.loop = true;
-      sourceRef.current = source;
+      sourceRef.current.buffer = buffer;
+      sourceRef.current.loop = true;
+      sourceRef.current.connect(analyzer);
       analyzer.connect(context.destination);
-      return source;
+      return sourceRef.current;
     },
+  };
+};
+
+const useFreqTimeData = () => {
+  const { analyzer, } = useAudioAnalyzer();
+  const freq = useRef(new Uint8Array(analyzer.frequencyBinCount));
+  const time = useRef(new Uint8Array(analyzer.frequencyBinCount));
+
+  useEffect(() => {
+    let stop = false;
+    const captureData = () => {
+      analyzer.getByteFrequencyData(freq.current); // <-- bar chart
+      analyzer.getByteTimeDomainData(time.current); // <-- waveform
+      if (!stop) requestAnimationFrame(captureData);
+    };
+
+    captureData();
+    return () => {
+      stop = true;
+    };
+  });
+
+  return {
+    freq: freq.current,
+    time: time.current,
   };
 };
 
 export const useMusik = () => {
   const useRefFunction = useRefFunctions();
   const { analyzer, context, } = useAudioAnalyzer();
-  const { connect, } = useAudioBufferSourceNode();
+  const { connect, source, } = useAudioBufferSourceNode();
+  const { freq, time, } = useFreqTimeData();
   const [ isLoading, setIsLoading, ] = useState(false);
 
+  const onAudioBuffer = useCallback((buffer: AudioBuffer) => {
+    connect(buffer).start(0);
+  }, [ connect, ]);
+
+  (window as any).source = source;
   return {
     isLoading,
+    play: () => source.start(),
+    pause: () => source.stop(),
     load: useRefFunction('load', (url: string) => {
       setIsLoading(true);
 
@@ -46,7 +84,7 @@ export const useMusik = () => {
         .then(response => response.arrayBuffer())
         .then(buffer => analyzer.context.decodeAudioData(buffer))
         .then(audioBuffer => {
-          connect(audioBuffer);
+          onAudioBuffer(audioBuffer);
           return audioBuffer;
         })
         .finally(() => setIsLoading(false));
