81f81ecd7ec2ea2a4bdd6d90bca8246f8238088c
Alex Eden <alexandereden91@gmail.com>
2020-08-04 20:41:01 -0500

    Rotate the rings

--- src/components/GeoRing/GeoRing.tsx
+++ src/components/GeoRing/GeoRing.tsx
@@ -1,11 +1,12 @@
-import React, { useMemo, useRef } from 'react';
-import { Group, MeshBasicMaterial } from 'three';
+import React, { useMemo, useRef, } from 'react';
+import { Group, MeshBasicMaterial, Mesh, } from 'three';
+import { useFrame, } from 'react-three-fiber';
 
 const radiusOut = 500;
 const radiusIn = 0.6 * radiusOut;
 
 export const GeoRing: React.FC = () => {
-  const shapes = (window as any).shapes = useRef<Group>();
+  const shapesRef = useRef<Group>();
   const material = useMemo(() => (
     new MeshBasicMaterial({
       color: 0xFFFFFF,
@@ -16,8 +17,16 @@ export const GeoRing: React.FC = () => {
     })
   ), []);
 
+  useFrame(() => {
+    const shapes = (shapesRef.current?.children ?? []) as Mesh[];
+
+    shapes.forEach(shape => {
+      shape.rotation.z += 0.01;
+    });
+  });
+
   return (
-    <group ref={shapes}>
+    <group ref={shapesRef}>
       <mesh material={material}>
         <ringBufferGeometry attach="geometry" args={[ radiusIn, radiusOut, 3, 1, 0, Math.PI * 2, ]} />
       </mesh>
