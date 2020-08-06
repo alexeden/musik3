661c7881620bb95f53b002b968bdf78fdcbcfa23
Alex Eden <alexandereden91@gmail.com>
2020-08-05 19:51:29 -0500

    Copy over wrapEffect utility function

---
+++ src/components/Effects/Effects.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Effects.scss';
+
+type Props = {
+
+};
+
+export const Effects: React.FC<Props> = ({
+
+}) => {
+  return (
+    <div className={styles.effectsWrapper}>
+      <h1>Effects</h1>
+    </div>
+  );
+};
---
+++ src/components/Effects/index.ts
@@ -0,0 +1,1 @@
+export { Effects as default, } from './Effects';
