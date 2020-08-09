92bf69321f64ea7d7af089a50644a2e95c5895af
Alex Eden <alexandereden91@gmail.com>
2020-08-08 22:17:08 -0500

    Clean up jss

--- src/components/MusikControls/MusikControls.tsx
+++ src/components/MusikControls/MusikControls.tsx
@@ -1,54 +1,7 @@
-import React, { useEffect, useCallback, useState, } from 'react';
-// import shallow from 'zustand/shallow';
+import React, { useCallback, useState, } from 'react';
 import { createUseStyles, } from 'react-jss';
-import { useMusikStore, useAudioLoaderStore, } from '../../store';
+import { useAudioLoaderStore, useMusikStore, } from '../../store';
 import SelectSong, { SONGS, } from '../SelectSong';
-
-const useStyles = createUseStyles({
-  controlBackdrop: {
-    pointerEvents: 'none',
-  },
-  controlWrapper: {
-    backdropFilter: 'blur(10px) brightness(5%) saturate(50) hue-rotate(60deg)',
-    pointerEvents: 'all',
-    minHeight: '450px',
-  },
-  controlHeaderWrapper: {
-    position: 'relative',
-    height: '350px',
-    width: '210px',
-    userSelect: 'none',
-  },
-  controlHeaderText: {
-    position: 'absolute',
-    height: '0',
-    lineHeight: '0',
-    top: '50%',
-    bottom: '50%',
-    width: '100%',
-    textAlign: 'center',
-    transform: 'rotate(-90deg)',
-    left: '-75px',
-    fontSize: '4rem',
-    letterSpacing: '-1px'
-    // backdropFilter: 'blur(10px) saturate(50) hue-rotate(45deg)',
-    // pointerEvents: 'all',
-  },
-  controlHeaderIcon: {
-    position: 'absolute',
-    height: '0',
-    lineHeight: '0',
-    top: '50%',
-    left: '25px',
-    fontWeight: '300',
-    bottom: '50%',
-    transform: 'rotate(90deg)',
-    fontSize: '12rem',
-    // transform: 'rotate(-90deg)',
-    // backdropFilter: 'blur(10px) saturate(50) hue-rotate(45deg)',
-    // pointerEvents: 'all',
-  },
-});
 
 export const MusikControls: React.FC = () => {
   const styles = useStyles();
@@ -64,31 +17,78 @@ export const MusikControls: React.FC = () => {
   }, [ audioLoader, musikActions, ]);
 
   return (
-    <div className={`fixed flex flex-row inset-0 z-10 items-center justify-center ${styles.controlBackdrop}`}>
+    <div className={'fixed flex flex-row inset-0 z-10 items-center justify-center pointer-events-none'}>
 
       {!isPlaying && (
-        <div className={`flex flex-row space-x-32 p-16 ${styles.controlWrapper}`}>
-          <div className={`${styles.controlHeaderWrapper}`}>
-            <h1 className={`text-6xl ${styles.controlHeaderText}`}>
-              musik
-            </h1>
-            <h1 className={`${styles.controlHeaderIcon}`}>
-              M
-            </h1>
+        <div className={`flex flex-row pointer-events-auto ${styles.controlWrapper}`}>
+          <div className={`relative select-none ${styles.controlHeaderWrapper}`}>
+            <h1 className={`${styles.controlHeaderText}`}>musik</h1>
+            <h1 className={`${styles.controlHeaderIcon}`}>M</h1> {/* It looks like a 3 I swear */}
           </div>
-          <SelectSong
-            onChange={song => setSelectedSong(song)}
-            song={selectedSong}
-          />
-          <div className={'flex flex-row justify-between'}>
-            <button className="text-2xl" onClick={load}>
-              {audioLoader.isLoading ? '...' : 'Load'}
-            </button>
-            <button className="text-2xl" onClick={() => play()}>Play</button>
 
+          <div className={`flex flex-col relative ${styles.controlFormWrapper}`}>
+            <SelectSong
+              onChange={song => setSelectedSong(song)}
+              song={selectedSong}
+            />
+            <div className={'flex flex-row justify-between'}>
+              <button className="text-2xl" onClick={load}>
+                {audioLoader.isLoading ? '...' : 'Load'}
+              </button>
+              <button className="text-2xl" onClick={() => play()}>Play</button>
+            </div>
           </div>
         </div>
       )}
     </div>
   );
 };
+
+const controlBlockBackdrop = (backdropFilter: string) => ({
+  backdropFilter: `${backdropFilter}`,
+  bottom: '-2rem',
+  content: '""',
+  left: '-2rem',
+  position: 'absolute',
+  right: '-2rem',
+  top: '-2rem',
+});
+
+const useStyles = createUseStyles({
+  controlWrapper: {
+    minHeight: '450px',
+  },
+  controlFormWrapper: {
+    margin: '2rem',
+    '&:before': { ...controlBlockBackdrop('blur(10px) brightness(5%) saturate(50) hue-rotate(20deg)'), },
+  },
+  controlHeaderWrapper: (props: any) => ({
+    margin: '2rem',
+    width: '210px',
+    '&:before': { ...controlBlockBackdrop('blur(10px) brightness(5%) saturate(50) hue-rotate(-20deg)'), },
+  }),
+  controlHeaderText: {
+    bottom: '50%',
+    fontSize: '4rem',
+    height: '0',
+    left: '-75px',
+    letterSpacing: '-1px',
+    lineHeight: '0',
+    position: 'absolute',
+    textAlign: 'center',
+    top: '50%',
+    transform: 'rotate(-90deg)',
+    width: '100%',
+  },
+  controlHeaderIcon: {
+    bottom: '50%',
+    fontSize: '12rem',
+    fontWeight: '300',
+    height: '0',
+    left: '25px',
+    lineHeight: '0',
+    position: 'absolute',
+    top: '50%',
+    transform: 'rotate(90deg)',
+  },
+});
