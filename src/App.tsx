169fe9ef977b9eeff3189d4a4b895fe8ff24e900
Alex Eden <alexandereden91@gmail.com>
2020-08-11 23:44:27 -0500

    Add <Stats> too App view

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Stats, } from 'drei';
 import MusikCanvas from './components/MusikCanvas';
 import MusikControls from './components/MusikControls';
 import Waveform from './components/Waveform';
@@ -8,5 +9,6 @@ export const App: React.FC = () => (
     <MusikControls />
     <MusikCanvas />
     <Waveform />
+    <Stats />
   </>
 );
